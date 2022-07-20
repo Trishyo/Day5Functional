22c786ed9a1479cc2802dc3d60dc3fcadec1b2b2
Trishyo <elnino.trishyo@gmail.com>
2022-07-20 17:27:23 +0530

    Added : Computing Quotient & Remainder Problem

---
+++ Compute_QnR.java
@@ -0,0 +1,16 @@
+package Day5Functional;
+
+public class Compute_QnR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int dividend = 58, divisor = 7;
+
+	    int quotient = dividend / divisor;
+	    int remainder = dividend % divisor;
+
+	    System.out.println("Quotient = " + quotient);
+	    System.out.println("Remainder = " + remainder);
+	}
+
+}
