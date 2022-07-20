725574879061ed810d7ac9558dd26072fe332fb9
Trishyo <elnino.trishyo@gmail.com>
2022-07-20 17:50:46 +0530

    Added : Vowel or Consonant Problem

---
+++ VowelOrConsonant.java
@@ -0,0 +1,21 @@
+package Day5Functional;
+
+public class VowelOrConsonant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'n';
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + " is vowel");
+                break;
+            default:
+                System.out.println(ch + " is consonant");
+	}
+		}
+			}
